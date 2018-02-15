3a501a472eb4259e48eb78df40698fd0c61f2f3e
xTobu <junxiang.oriint@gmail.com>
2018-02-15 15:28:48 +0800

    update config

---
+++ backend/server/router/handlers/api/mssqldb/config.go
@@ -0,0 +1,22 @@
+package mssqldb
+
+//IsDebug  massql data
+var IsDebug = true
+
+//Server  massql data
+var Server = "211.78.85.202"
+
+//User  massql data
+var User = "junxiang"
+
+//Password  massql data
+var Password = "WPE8yxwq"
+
+//Database  massql data
+var Database = "junxiang_db"
+
+//Encrypt  massql data
+var Encrypt = "disable"
+
+//ConnectionStr  ./router/sqldb/config.go
+var ConnectionStr = "server=211.78.85.202;database=junxiang_db;user id=junxiang;password=WPE8yxwq;encrypt=disable;"
---
+++ backend/server/router/handlers/api/mysqldb/config.go
@@ -0,0 +1,10 @@
+package mysqldb
+
+//ConnectionStr 本機  mysqldb/config.go
+//var ConnectionStr = "Junxiang:rmp4vu;6@tcp(127.0.0.1:3306)/school"
+
+//ConnectionStr 敦偉 mysqldb/config.go
+//var ConnectionStr = "junxiang:feQ8EDS8@tcp(211.78.85.201:3306)/junxiang_db"
+
+//ConnectionStr CloudSQL mysqldb/config.go
+var ConnectionStr = "Junxiang:rmp4vu;6@tcp(35.229.226.124:3306)/junxiang_db"
